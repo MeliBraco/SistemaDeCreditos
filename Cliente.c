a78738294bfaf223f62463adfb2164f74dd6a926
MelinaB <32472302+MeliBraco@users.noreply.github.com>
2019-06-03 17:32:26 -0300

    Metodo FilePath (Dependiendo del ID del cliente se escribe en un determinado archivo)

--- Cliente.c
+++ Cliente.c
@@ -29,7 +29,25 @@ void ingresar(){
 
     strcpy(nuevoCliente.nombre, nombreAux);
 
-   // FilePath(&nuevoCliente);
+    FilePath(&nuevoCliente);
 
     printf("El Empleado fue guardado correctamente \n\n");
+}
+
+void FilePath(struct Cliente *nuevoCliente)
+{
+    //Elige en que archivo se va a escribir los datos del clinte segun ID
+
+    FILE * archivo;
+
+    if(nuevoCliente->Id > 0 && nuevoCliente->Id <= 500 ){
+
+        archivo = fopen("Cliente0_500.txt", "a");
+    }
+    else if(nuevoCliente->Id > 500 && nuevoCliente->Id <= 1000){
+
+        archivo = fopen("Cliente500_1000.txt", "a");
+    }
+
+    //guardar(&nuevoCliente ,archivo);
 }
